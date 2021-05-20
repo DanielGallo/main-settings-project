81de7cc40f90317784e138e1d07ac38485e1efb7
danielgallo <daniel.gallo@gmail.com>
2021-05-20 11:14:31 +0100

    Update shared.kt

--- shared.kt
+++ shared.kt
@@ -1,5 +1,7 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 fun createDeployBuildConfiguration(vcRoot: GitVcsRoot): BuildType {
     val deploy = BuildType {
